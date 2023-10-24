8ce2f5e7ca38243c39d474b8759b692be8685bf7
Grant C <70045551+ForceOverArea@users.noreply.github.com>
2023-10-23 22:52:22 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -6,9 +6,8 @@
 Result<float, const char *> safe_division(float num, float denom)
 {
     using Output = Result<float, const char *>;
-
     if (denom == 0.0f)
-        return Output((const char *)"ligma balls");
+        return Output((const char *)"error (safe_division): result is undefined...");
     else
         return Output((float)(num / denom));
 }
@@ -16,26 +15,23 @@ Result<float, const char *> safe_division(float num, float denom)
 Result<float, const char *> safe_sqrt(float radicand)
 {
     using Output = Result<float, const char *>;
-
     if (radicand < 0.0f)
-        return Output((const char *)"also ligma balls");
+        return Output((const char *)"error (safe_sqrt): result is inaginary...");
     else 
         return Output((float)std::sqrt(radicand));
 }
 
 int main()
 {
-
-    float successful_result = safe_division(1, 2)
+    Result<float, const char *> successful_result = safe_division(1, 2)
         .apply<float>(safe_sqrt)
-        .apply<float>([](float x){ return safe_division(x, 4.0f); })
-        .unwrap();
+        .apply<float>([](float x){ return safe_division(x, 4.0f); });
 
     Result<float, const char *> unsuccessful_result = safe_division(1, 0)
         .apply<float>(safe_sqrt)
         .apply<float>([](float x){ return safe_division(x, 4.0f); });
 
-    std::cout << "Successful Result: " << successful_result << std::endl;
+    std::cout << "Successful Result: " << successful_result.unwrap() << std::endl;
 
     std::cout << "Unsuccessful Result: " << unsuccessful_result.unwrap_err() << std::endl;
 
